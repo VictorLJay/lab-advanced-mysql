689a3e4e47411433fe178bb50ad0e78120d123c1
VictorLJay <victor.lj1995@gmail.com>
2020-11-11 19:23:58 +0100

    lab corrected

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -5,130 +5,118 @@ Challenge 1 - Most Profiting Authors
 */
 
 SELECT * FROM sales; -- title_id, qty
-SELECT * FROM authors; -- au_id
 SELECT * FROM titles; -- title_id, royalty, price, advance
 SELECT * FROM titleauthor; -- au_id, title_id, royaltyper
 
-
 -- Step 1: Calculate the royalty of each sale for each author and the advance for each author and publication
-SELECT 
-    t.title_id,
-    ta.au_id,
-    (t.advance * ta.royaltyper / 100) AS advance,
-    (t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) AS sales_royalty
-FROM
-    titles t
-        INNER JOIN
-    titleauthor ta ON t.title_id = ta.title_id
-        INNER JOIN
-    sales s ON t.title_id = s.title_id;
+SELECT titleauthor.au_id, titleauthor.title_id, titles.advance * titleauthor.royaltyper / 100 as advance
+FROM titleauthor
+JOIN titles
+ON titleauthor.title_id = titles.title_id;
+
+SELECT titleauthor.au_id, titleauthor.title_id, 
+	SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty
+FROM titleauthor
+JOIN sales
+ON titleauthor.title_id = sales.title_id
+JOIN titles
+ON titleauthor.title_id = titles.title_id
+GROUP BY titleauthor.au_id, titleauthor.title_id;
 
 -- Step 2: Aggregate the total royalties for each title and author
-SELECT 
-    agg_royalty.title_id,
-    agg_royalty.au_id,
-    SUM(advance) AS total_advance,
-    SUM(sales_royalty) AS total_sales_royalty
-FROM
-    (SELECT 
-        t.title_id,
-            ta.au_id,
-            (t.advance * ta.royaltyper / 100) AS advance,
-            (t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) AS sales_royalty
-    FROM
-        titles t
-    INNER JOIN titleauthor ta ON t.title_id = ta.title_id
-    INNER JOIN sales s ON t.title_id = s.title_id) agg_royalty
-GROUP BY agg_royalty.title_id , agg_royalty.au_id;
+SELECT titleauthor.au_id, titleauthor.title_id, 
+	titles.advance * titleauthor.royaltyper / 100 as advance,
+    royalties.sales_royalty
+FROM titleauthor
+JOIN titles
+ON titleauthor.title_id = titles.title_id
+JOIN
+	(SELECT titleauthor.au_id, titleauthor.title_id, 
+		SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty
+	FROM titleauthor
+	JOIN sales
+	ON titleauthor.title_id = sales.title_id
+	JOIN titles
+	ON titleauthor.title_id = titles.title_id
+	GROUP BY titleauthor.au_id, titleauthor.title_id) royalties
+ON titleauthor.au_id = royalties.au_id and titleauthor.title_id = royalties.title_id
+GROUP BY titleauthor.au_id, titleauthor.title_id;
 
 -- Step 3: Calculate the total profits of each author
-SELECT 
-    total_profits.au_id,
-    ROUND(SUM(total_profits.total_advance + total_profits.total_sales_royalty),
-            2) AS profit_per_author
-FROM
-    (SELECT 
-        agg_royalty.title_id,
-            agg_royalty.au_id,
-            SUM(agg_royalty.advance) AS total_advance,
-            SUM(agg_royalty.sales_royalty) AS total_sales_royalty
-    FROM
-        (SELECT 
-        t.title_id,
-            ta.au_id,
-            (t.advance * ta.royaltyper / 100) AS advance,
-            (t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) AS sales_royalty
-    FROM
-        titles t
-    INNER JOIN titleauthor ta ON t.title_id = ta.title_id
-    INNER JOIN sales s ON t.title_id = s.title_id) agg_royalty
-    GROUP BY agg_royalty.title_id , agg_royalty.au_id) total_profits
-GROUP BY total_profits.au_id
-ORDER BY profit_per_author DESC
+SELECT titleauthor.au_id, titleauthor.title_id, 
+	SUM(titles.advance * titleauthor.royaltyper / 100 +
+    royalties.sales_royalty) as total
+FROM titleauthor
+JOIN titles
+ON titleauthor.title_id = titles.title_id
+JOIN
+	(SELECT titleauthor.au_id, titleauthor.title_id, 
+		SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty
+	FROM titleauthor
+	JOIN sales
+	ON titleauthor.title_id = sales.title_id
+	JOIN titles
+	ON titleauthor.title_id = titles.title_id
+	GROUP BY titleauthor.au_id, titleauthor.title_id) royalties
+ON titleauthor.au_id = royalties.au_id and titleauthor.title_id = royalties.title_id
+GROUP BY titleauthor.au_id
+ORDER BY total DESC
 LIMIT 3;
 
 /*
 Challenge 2 - Alternative Solution
 */
 -- Create the first temporary table
-DROP TABLE royal_agg;
-CREATE TEMPORARY TABLE royal_agg
-SELECT 
-    t.title_id,
-    ta.au_id,
-    (t.advance * ta.royaltyper / 100) AS advance,
-    (t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) AS sales_royalty
-FROM
-    titles t
-        INNER JOIN
-    titleauthor ta ON t.title_id = ta.title_id
-        INNER JOIN
-    sales s ON t.title_id = s.title_id;
-   
--- Check the first table
-SELECT * FROM royal_agg;
+SELECT titleauthor.au_id, titleauthor.title_id, titles.advance * titleauthor.royaltyper / 100 as advance
+FROM titleauthor
+JOIN titles
+ON titleauthor.title_id = titles.title_id;
+
+DROP TABLE royalties;
+CREATE TEMPORARY TABLE royalties
+SELECT titleauthor.au_id, titleauthor.title_id, 
+	SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as sales_royalty
+FROM titleauthor
+JOIN sales
+ON titleauthor.title_id = sales.title_id
+JOIN titles
+ON titleauthor.title_id = titles.title_id
+GROUP BY titleauthor.au_id, titleauthor.title_id;
 
 -- Create the second temporary table
-DROP TABLE total_profits;
-CREATE TEMPORARY TABLE total_profits
-SELECT ra.title_id, ra.au_id, SUM(ra.advance) AS total_advance, SUM(ra.sales_royalty) AS sales_royalty 
-FROM royal_agg ra
-GROUP BY ra.title_id, ra.au_id;
+DROP TABLE royalty_plus_advance;
+CREATE TEMPORARY TABLE royalty_plus_advance
+SELECT titleauthor.au_id, titleauthor.title_id, 
+	titles.advance * titleauthor.royaltyper / 100 as advance,
+    royalties.sales_royalty
+FROM titleauthor
+JOIN titles
+ON titleauthor.title_id = titles.title_id
+JOIN royalties
+ON titleauthor.au_id = royalties.au_id and titleauthor.title_id = royalties.title_id
+GROUP BY titleauthor.au_id, titleauthor.title_id;
 
--- Check the second table
-SELECT * FROM total_profits;
-
--- Create the final table
-SELECT 
-    tp.au_id,
-    ROUND(SUM(tp.total_advance + tp.sales_royalty),
-            2) AS total_revenue
-FROM
-    total_profits tp
-GROUP BY tp.au_id
-ORDER BY total_revenue DESC
+-- Getting the final result
+SELECT au_id, 
+	SUM(advance + sales_royalty) as total
+FROM royalty_plus_advance
+GROUP BY au_id
+ORDER BY total DESC
 LIMIT 3;
 
 /*
 Challenge 3
 */
--- Creating the temporary table that will turn into a permanent table
-DROP TABLE profitable_authors;
-CREATE TEMPORARY TABLE profitable_authors
-SELECT 
-    tp.au_id,
-    ROUND(SUM(tp.total_advance + tp.sales_royalty),
-            2) AS total_revenue
-FROM
-    total_profits tp
-GROUP BY tp.au_id
-ORDER BY total_revenue DESC
+-- First create a temporary table from the table below, and then create a table from it.
+DROP TABLE total;
+CREATE TEMPORARY TABLE total
+SELECT au_id, 
+	SUM(advance + sales_royalty) as total
+FROM royalty_plus_advance
+GROUP BY au_id
+ORDER BY total DESC
 LIMIT 3;
 
-
-DROP TABLE most_profiting_authors;
+-- Table creation
 CREATE TABLE most_profiting_authors
-SELECT * FROM profitable_authors;
-
-SELECT * FROM most_profiting_authors;
-
+SELECT * FROM total;
